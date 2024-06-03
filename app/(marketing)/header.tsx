d2f2c1ef3c8972c0f2d58d79bb2a07cecd40f416
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-06-03 16:52:15 +0200

    auth setup & landing ui upd
    - set up authentication with Clerk;
    - created header and footer components for marketing/landing layout;
    - updated marketing/landing page with image, heading and specific auth-buttons;

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,7 +1,45 @@
+import Image from "next/image";
+import {
+  ClerkLoading,
+  ClerkLoaded,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
 export const Header = () => {
   return (
     <header className="h-20 w-full border-b-2 border-slate-200 px-4">
-      Header
+      <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="/mascot.svg" alt="Mascot" width={40} height={40} />
+          <span className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </span>
+        </div>
+
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+
+        <ClerkLoaded>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+
+          <SignedOut>
+            <SignInButton mode="modal">
+              <Button size="lg" variant="ghost">
+                Login
+              </Button>
+            </SignInButton>
+          </SignedOut>
+        </ClerkLoaded>
+      </div>
     </header>
   );
 };
