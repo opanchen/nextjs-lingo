bd098b9db172a564c5cb199f9a9e53683e32be6b
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-06-03 13:59:35 +0200

    create marketing/landing layout skeleton

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,16 @@
+import { Header } from "./header";
+import { Footer } from "./footer";
+
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1 flex flex-col items-center justify-center">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default MarketingLayout;
