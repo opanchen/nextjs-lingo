447065f6e48ca4cd9858a427ea4c5053f924e86b
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-06-05 15:27:37 +0200

    add audio files & quiz footer
    - implemented audio-sound on challenge-card click using 'react-use' package and .mp3 files from elevenlabs.io;
    - created footer component for quizzes;

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -7,6 +7,7 @@ import { challengeOptions, challenges } from "@/db/schema";
 import { Header } from "./header";
 import { QuestionBubble } from "./question-bubble";
 import { Challenge } from "./challenge";
+import { Footer } from "./footer";
 
 type Props = {
   initialLessonId: number;
@@ -36,8 +37,17 @@ export const Quiz = ({
     return uncompletedIndex === -1 ? 0 : uncompletedIndex;
   });
 
+  const [selectedOption, setSelectedOption] = useState<number>();
+  const [status, setStatus] = useState<"correct" | "wrong" | "none">("none");
+
   const challenge = challenges[activeIndex];
   const options = challenge?.challengeOptions || [];
+
+  const onSelect = (id: number) => {
+    if (status !== "none") return;
+
+    setSelectedOption(id);
+  };
 
   const title =
     challenge.type === "ASSIST"
@@ -66,9 +76,9 @@ export const Quiz = ({
 
               <Challenge
                 options={options}
-                onSelect={() => {}}
-                status="none"
-                selectedOption={undefined}
+                onSelect={onSelect}
+                status={status}
+                selectedOption={selectedOption}
                 disabled={false}
                 type={challenge.type}
               />
@@ -76,6 +86,8 @@ export const Quiz = ({
           </div>
         </div>
       </div>
+
+      <Footer disabled={!selectedOption} status={status} onCheck={() => {}} />
     </>
   );
 };
