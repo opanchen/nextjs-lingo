be3bb811d8071602eca5c6b38f9343fa0accff26
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-06-15 07:51:11 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,10 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher(["/", "/api/webhooks/stripe"]);
 
-// TODO: publicRoutes: ["/", "/api/webhooks/stripe"]
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) auth().protect();
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
